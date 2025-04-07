e22327bc8e225cb1dc075922f2a6f40f43d0b5f4
Robinson Garcia <edifactmxrobinsongarcia@gmail.com>
2025-04-07 12:39:41 -0600

    version inicial

---
+++ src/main/java/mx/com/edifact/merida/gitdemoproject/GitDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.edifact.merida.gitdemoproject;
+
+/**
+ *
+ * @author rgarcia
+ */
+public class GitDemo {
+    
+    public static void main(String[] args) {
+        String holaMundo = "Hola mundo!";
+        String nombre = "Robinson";
+        int edad = 15;
+        System.out.println(holaMundo + ". Mi edad es "+ edad);
+    }
+}
